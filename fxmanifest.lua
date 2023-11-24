6a7bf9fe2552f6baf13c2c3fd3eb40e55d62e58a
Achraf <136148902+achrafvod@users.noreply.github.com>
2023-11-24 16:00:33 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AchrafV'
+description 'Fonts Arabic'
+version '1.0.0'
+url 'https://github.com/achrafvod/arabic-fonts-fivem'
+
+client_script 'demo.lua'
