4e9b30d284115bd1fda898f113f88a1e39fcc131
Achraf <136148902+achrafvod@users.noreply.github.com>
2023-11-24 15:56:36 +0100

    Create demo.lua

---
+++ demo.lua
@@ -0,0 +1,11 @@
+RegisterFontFile('space')
+fontId = RegisterFontId('space')
+SetTextFont(fontId)
+
+RegisterFontFile('A9eelsh')
+fontId = RegisterFontId('A9eelsh')
+SetTextFont(fontId)
+
+RegisterFontFile('sharlock')
+fontId = RegisterFontId('sharlock')
+SetTextFont(fontId)
